4f269ada05b6518457fef5b6de3505fa8532b540
NanoBlanco <reynaldojblancom@gmail.com>
2024-05-21 17:33:43 -0400

    Creado Modelos Usuario y Rol

--- src/main/java/com/desarrollo/adopcion/modelo/Rol.java
+++ src/main/java/com/desarrollo/adopcion/modelo/Rol.java
@@ -1,5 +1,52 @@
 package com.desarrollo.adopcion.modelo;
 
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.List;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Rol {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	private String rol;
+	
+	@ManyToMany(mappedBy="roles")
+	private Collection<Usuario> usuarios = new HashSet<>();
+	
+	public void assignRolToUser(Usuario usr) {
+		usr.getRoles().add(this);
+		this.getUsuarios().add(usr);
+	}
+	
+	public void removeUserFromRol(Usuario usr) {
+		usr.getRoles().remove(this);
+		this.getUsuarios().remove(usr);
+	}
+	
+	public void removeAllUsersFromRol() {
+		if(this.getUsuarios()!=null) {
+			List<Usuario> rolUsuarios = this.getUsuarios().stream().toList();
+			rolUsuarios.forEach(this :: removeUserFromRol);
+		}
+	}
+
+	public Rol(String rolName) {
+		this.rol = rolName;
+	}
 
 }
--- src/main/java/com/desarrollo/adopcion/modelo/Usuario.java
+++ src/main/java/com/desarrollo/adopcion/modelo/Usuario.java
@@ -1,5 +1,40 @@
 package com.desarrollo.adopcion.modelo;
 
+import java.util.Collection;
+import java.util.HashSet;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Usuario {
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private Long id;
+	private String nombre;
+	private String apellido;
+	private String correo;
+	private String clave;
+	private boolean estado;
+	
+	@ManyToMany(fetch = FetchType.EAGER, cascade = {CascadeType.PERSIST, CascadeType.MERGE, CascadeType.DETACH})
+	@JoinTable(name="usuario_roles", 
+	joinColumns = @JoinColumn(name="user_id", referencedColumnName = "id"), 
+	inverseJoinColumns = @JoinColumn(name="role_id", referencedColumnName="id"))
+	private Collection<Rol> roles = new HashSet<>();
 
 }
